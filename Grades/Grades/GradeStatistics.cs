e32dd21ca8d68f774edc639fd06d90d706cd6b02
Afshar, Arash <arash.afshar@gatech.edu>
2017-07-31 23:39:37 -0400

    Section 4: Assemblies
    References, unit tests, and test cases

--- Grades/Grades/GradeStatistics.cs
+++ Grades/Grades/GradeStatistics.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Grades
+﻿namespace Grades
 {
-    class GradeStatistics
+    public class GradeStatistics
     {
         public GradeStatistics()
         {
